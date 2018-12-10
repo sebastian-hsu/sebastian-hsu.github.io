6441ac904649b459636799b1d4105936af6a586d
sebastian <sebastian.hsu@gmail.com>
2018-12-10 08:57:12 +0800

    remove more unnecessary files

--- wp-content/advanced-cache.php
+++
@@ -1,9 +0,0 @@
-<?php
-// Exit if accessed directly
-if ( ! defined( 'ABSPATH' ) ) {
-	exit;
-} 
-
-if( defined( 'WP_CACHE' ) && WP_CACHE === true && defined( 'WP_STATIC_CACHE_PLUGIN_HOME' ) )
-	WP_DEBUG ? include_once( WP_STATIC_CACHE_PLUGIN_HOME . 'static-cache.php' ) : @include_once( WP_STATIC_CACHE_PLUGIN_HOME . 'static-cache.php' );
-?>
--- wp-content/plugins/wp-static-cache/static-cache.php
+++
@@ -1,9 +0,0 @@
-<?php
-// Exit if accessed directly
-if ( ! defined( 'ABSPATH' ) ) {
-	exit;
-} 
-
-include_once "core/ecpt-wp-static-cache-mobile-user-agents.php"; // is from mobile?
-include_once "core/core.php";
-?>
